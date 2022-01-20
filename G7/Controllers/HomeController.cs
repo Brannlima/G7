22becafeaff2070c8025ab8b69876706abe0ae27
Brannlima <braanlennon@gmail.com>
2022-01-19 23:02:34 -0300

    Initial Commit

---
+++ G7/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using G7.Models;
+using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace G7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Branderson Lima";
+
+            return View();
+        }
+
+        public ActionResult Contact(string nome, string email, string msg, string tel)
+        {
+            FormData formData = new FormData
+            {
+                Name = nome,
+                Email = email,
+                Tel = tel,
+                Text = msg
+            };
+
+            string path = @"c:\temp\G7Form.txt";
+            if (!System.IO.File.Exists(path))
+            {
+                using (StreamWriter sw = System.IO.File.CreateText(path))
+                {
+                    sw.WriteLine("Nome: " + nome);
+                    sw.WriteLine("Email: " + email);
+                    sw.WriteLine("Telefone: " + tel);
+                    sw.WriteLine("Mensagem: " + msg);
+                }
+            }
+            else
+            {
+                string text = "Nome: " + nome + Environment.NewLine +
+                              "Email: " + email + Environment.NewLine +
+                              "Telefone: " + tel + Environment.NewLine +
+                              "Mensagem: " + msg;
+                System.IO.File.WriteAllText(path, text);
+            }
+            return View();
+        }
+    }
+}
---
+++ G7/Models/FormData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace G7.Models
+{
+    public class FormData
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Text { get; set; }
+        public string Tel { get; set; }
+    }
+}
